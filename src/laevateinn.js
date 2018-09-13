eccfd83ba0cf91f832eeb1030c187b5475037349
Kuma Li <li.jun.kuma@gmail.com>
2018-09-13 15:10:08 +0800

    doc: remove unused comment

--- src/laevateinn.js
+++ src/laevateinn.js
@@ -3,11 +3,7 @@ import * as Dialog from './utils/os/dialog';
 
 export function exportAction(){
   const doc = context.document;
-  // OSUI.floatPanel();
   if (doc) {
-    // const alert = utils.createDialog()[0];
-    // alert.runModal();
-    // const res = utils.saveFile(context.document);
     const res = color.generateFiles(doc);
     if (res) {
       Dialog.alert('Congratulation!', 'Export Design Token Succeed!');
